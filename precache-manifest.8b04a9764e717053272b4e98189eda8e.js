3c00d4f308a25f5e6ab79161436d7c937f0cfdc5
Alex Ian <alexian@Alexde-MacBook-Pro.local>
2019-11-25 12:25:09 +0800

    Rebuild website

---
+++ precache-manifest.8b04a9764e717053272b4e98189eda8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0451fc5c9ed3e9035cb2665a2338d8",
+    "url": "/sw-app/index.html"
+  },
+  {
+    "revision": "0562741f34a4e7d20512",
+    "url": "/sw-app/static/css/main.f0b8734e.chunk.css"
+  },
+  {
+    "revision": "3a5dec4c46a43f587b69",
+    "url": "/sw-app/static/js/2.5e3ba46b.chunk.js"
+  },
+  {
+    "revision": "0562741f34a4e7d20512",
+    "url": "/sw-app/static/js/main.a9c4bcf5.chunk.js"
+  },
+  {
+    "revision": "05cf50ebcc19588ac34e",
+    "url": "/sw-app/static/js/runtime-main.ce51584e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sw-app/static/media/logo.25bf045c.svg"
+  }
+]);
